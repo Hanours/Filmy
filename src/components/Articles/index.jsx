77ab558de91e28db9f1ef5380ae3891796d4f5f5
Hana Sedlaříková <hana.sedlarikova@gmail.com>
2023-04-06 20:02:08 +0200

    Přidání menu, articles, reviews

---
+++ src/components/Articles/index.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import './style.css';
+
+
+const Articles = () => (
+
+    <>
+        <h1>Články</h1>
+        
+        <p> Vítejte na našem webu o filmech, kde najdete nejnovější zprávy a recenze o filmech, hercích a herečkách. 
+            Naše stránka nabízí rozsáhlou kolekci článků a recenzí o filmech z různých žánrů - od nejnovějších blockbusterů až po kultovní klasiky.</p>
+
+        <p>Snažíme se poskytnout návštěvníkům našeho webu kvalitní a nezaujaté recenze, které vám pomohou rozhodnout se, jaký film si vybrat na svůj večer. 
+            Navíc nabízíme exkluzivní rozhovory s herci a režiséry, takže se můžete dozvědět více o tvorbě a vývoji nejnovějších filmů.</p>
+
+        <p>Naše stránka také obsahuje články o historii filmu, 
+         filmové techniky a zajímavosti o filmových kulisech, které vám poskytnou ucelený pohled na filmový svět.</p>
+    </>
+)
+
+
+export default Articles;
---
+++ src/components/Home/index.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import './style.css';
+
+const Home = () => (
+
+    <>
+        <h1>Vítejte na našem webu s filmy!</h1>
+        <p> Najdete zde širokou škálu filmů různých žánrů, od akčních thrillerů až po romantické komedie. 
+            Naše kolekce obsahuje tisíce filmů, ať už se jedná o nejnovější trháky nebo klasické filmy, které si můžete znovu a znovu vychutnat.</p>
+
+        <p>Naše stránka je snadno použitelná a přehledná, díky čemuž se rychle dostanete k filmům, které hledáte. 
+            A pro ty, kteří chtějí být v obraze, jsou zde i články o nejnovějších filmech, herečkách a hercích.</p>
+
+        <p>Přidejte se k nám a užijte si skvělé filmy s rodinou a přáteli nebo se jednoduše zrelaxujte po náročném dni. 
+            Nezapomeňte, že si můžete nastavit svůj účet a přidat si své oblíbené filmy, které se budou zobrazovat na vaší osobní stránce.
+            Nyní zahajte svou filmovou cestu a užijte si svůj oblíbený film na našem webu.</p>
+    </>
+)
+
+
+export default Home;
